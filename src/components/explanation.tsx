2b8b53048373a160add981979945c9eab2c105d6
Bjarne <bjarne.abb@gmail.com>
2023-06-12 16:59:20 +0200

    render explanation string as html

--- src/components/explanation.tsx
+++ src/components/explanation.tsx
@@ -7,7 +7,12 @@ interface explanation {
 const Explanation: React.FC<explanation> = ({ explanation }) => {
     return <div className={"p-8 text-white tracking-wide w-full max-w-lg"}>
         <p className="break-word pb-4 font-semibold whitespace-pre-wrap text-white">Task Explanation</p>
-        {explanation != "" ? explanation : "No Explanation."}
+        {explanation != "" ?
+            <div dangerouslySetInnerHTML={{ __html: explanation }}></div>
+            :
+            "No Explanation."
+        }
+
     </div>
 }
 
