969b78301800f498fd7f90adbeb223480e9120fe
Jan <jlbichel@gmail.com>
2023-06-23 13:03:12 +0200

    Explanations Edited
    *UI Explanations added
    *Formatted many explanations with headers and linebreaks

--- src/data/levels.tsx
+++ src/data/levels.tsx
@@ -2,7 +2,22 @@ import type { ILevel } from "../types/karel";
 export const levels: Array<ILevel> = [
     {
         name: "Move the Beeper LT - Seeing Commands",
-        explanation: `TO DO: GOAL CONDITION ERKLÄREN; UI ERKLÄREN <h1>Willkommen beim Learnlab!</h1><p>Rechts siehst du ein Spielfeld mit einem blauen Pfeil darin, das ist Pfeili. Wenn du auf "Run Code" klickst, dann wird Pfeili die Befehle weiter unten ausführen. Dein Ziel ist es die roten Beeper aufzusammeln und in den markierten Zielzonen wieder abzulegen. Drücke auf "Run Code" und sieh was passiert.</p>`,
+        explanation: `<b>Willkommen beim Learnlab!</b><br>
+        Dies ist ein Spiel, mit dem Du Programmieren lernen kannst. Die verwendete Programmiersprache trägt den Namen JavaScript, aber die erlernten Konzepte finden sich auch in allen anderen Programmiersprachen wieder.<br><br>
+        
+        <b>Der Code-Editor</b><br>
+        In der Mitte siehst du ein großes Textfeld, in das Du Anweisungen schreiben kannst.<br><br>
+        
+        <b>Das Spielfeld</b><br>
+        Rechts davon ist ein Spielfeld mit einem blauen Pfeil darin, das ist Pfeili. Wenn du auf den Play-Knopf unter dem Spielfeld klickst, dann wird Pfeili die Anweisungen im Textfeld ausführen.<br><br>
+        
+        <b>Das Ziel</b><br>
+        Jedes Level ist ein Puzzle. Um es zu lösen, musst du Anweisungen in das Textfeld schreiben und diese danach ausführen. Dabei müssen zwei Bedingungen erfüllt werden:<br> 
+        1. Alle Beeper außerhalb von Zielzonen (Die roten Rauten) müssen aufgesammelt werden.<br>
+        2. In allen markierten Zielzonen müssen Beeper abgelegt werden (diese erscheinen dann als grüne Rauten).<br><br>
+        
+        <b>Los geht's</b><br>
+        In diesem Fall wurden die korrekten Anweisungen bereits eingefügt. Drücke auf den Play-Knopf und sieh was passiert.`,
         code: `move();
 pickBeeper();
 turnLeft();
@@ -29,17 +44,36 @@ putBeeper();`,
     },
     {
         name: "Move the Beeper LT - Using commands",
-        explanation: `Nun musst du selbst Befehle einfügen, um den roten Beeper aufzuheben und ihn in der Zielzone abzulegen. Links siehst du die verfügbaren Befehle. Du kannst diese entweder per Hand weiter unten in dieses Textfeld schreiben, oder sie links in der Liste anklicken, um sie direkt einzufügen.`,
-        code: `//Mit move() bewegst du dich ein Feld vorwärts
-//in aktuelle Blickrichtung.
-//Mit turnLeft() drehst du Pfeili 90° gegen den
-//Uhrzeigersinn.
-//Mit putBeeper() kannst du einen Beeper auf dem Feld
-//ablegen, auf dem sich Pfeili befindet.
-//Mit pickBeeper() kannst du Beeper aufheben, wenn
-//sich Pfeili auf dem selben Feld mit einem Beeper befindet.
+        explanation: `Nun musst du selbst Befehle einfügen, um den roten Beeper aufzuheben und ihn in der Zielzone abzulegen. Links siehst du die verfügbaren Befehle. Du kannst diese entweder per Hand weiter unten in dieses Textfeld schreiben, oder sie links in der Liste anklicken, um sie direkt einzufügen.<br><br>
 
-move();
+        <b>Die Befehle</b><br>
+        <em>move()</em> Pfeili bewegt sich ein Feld vorwärts
+        in aktueller Blickrichtung.<br>
+        <em>turnLeft()</em> Pfeili dreht sich 90° gegen den
+        Uhrzeigersinn.<br>
+        <em>putBeeper()</em> Pfeili legt einen Beeper auf dem Feld ab, auf dem er sich momentan befindet (sofern Pfeili noch genügend Beeper im Rucksack hat).<br>
+        <em>pickBeeper()</em> Pfeili hebt einen Beeper  vom momentanen Feld auf (sofern sich dort einer befindet).<br><br>
+
+        <b>Fehlschlag</b><br>
+        <em>Ungültiger Befehl:</em> Versuchst du einen Befehl auszuführen, der in dieser Situation nicht ausführbar ist (zum Beispiel wenn du versuchst, durch eine Wand zu laufen), wird die Ausführung des Programms abbrechen. Dann setzt du das Level zurück auf Anfang und versuchst es erneut.<br>
+        <em>Ziel nicht erreicht:</em> Wenn alle Befehle ausgeführt worden sind und das Ziel des Levels noch nicht erreicht wurde, dann kannst du das Level genauso auf Anfang zurücksetzen.<br><br>
+
+
+        <b>Die linke Menüleiste</b><br>
+        Ganz links am Rand ist eine vertikale Leiste mit drei Menüpunkten:<br>
+        1. <em>Der Erklärungstext.</em> Hier befindest Du dich gerade.<br>
+        2. <em>Die verfügbaren Befehle.</em> Dies sind die grundlegenden Befehle, welche für Pfeili zur Verfügung stehen. Diese können zwischen den Leveln variieren.<br>
+        3. <em>Der Command-Log.</em> Hier werden die von Pfeili ausgeführten Befehle in einer Liste mitgeschrieben. Wenn es ein Fehler mit dem Programm gibt, dann wird dieser hier erscheinen.<br><br>
+        
+        <b>Die Knöpfe unter dem Spielfeld</b><br>
+        1. <em>Wiedergabegeschwindigkeiten</em>: Es gibt insgesamt vier Wiedergabegeschwindigkeiten (wenn man den Pause-Knopf mitrechnet).<br>
+        2. <em>Nächster Schritt</em>: Rechts vom Pause-Knopf gibt einen Knopf, mit dem immer nur der jeweils nächste Befehl ausgeführt wird. Drücke dafür erst auf den Pause-Knopf und dann so oft du willst auf den Nächster-Schritt-Knopf.<br>
+        3. <em>Zurücksetzen</em>: Der Knopf mit dem kreisförmigen Pfeil setzt das Level in den Ausgangszustand zurück.
+        4. <em>Fortschrittsanzeige</em>: Das Symbol ganz rechts in der Reihe von Knöpfen kann man nicht drücken. Es zeigt Dir nur an, ob du das momentan ausgewählte Level schon abgeschlossen hast.<br><br>
+        
+        <b>Die Knöpfe über dem Spielfeld</b><br>
+        Über dem Spielfeld wird der Name des Aktuellen Levels angezeigt. Die Kannst die Pfeile links und rechts des Namens benutzen, um die vorherigen oder nachfolgenden Level auszuwählen. Du kannst auch direkt auf den Namen des Levels klicken, um eine Liste mit allen Leveln anzuzeigen.<br><br>`,
+        code: `move();
 pickBeeper();
 //hier drunter weiteren Code einfügen...`,
         commands: ["move", "turnLeft", "putBeeper", "pickBeeper"],
@@ -86,7 +120,10 @@ pickBeeper();
     },
     {
         name: "Move the Beeper RT1 - Seeing Functions",
-        explanation: `Man kann also eine Rechtsdrehung aus 3 Linksdrehungen erzeugen. Wenn du die Rechtsdrehung auf diese Weise häufiger nutzt, dann wird es allerdings unübersichtlich im Code. Um das zu vermeiden, kann man sogenannte "Funktionen" schreiben. Diese bündeln eine beliebige  Abfolge bereits vorhandener Befehle unter einem neuen Namen zusammen.`,
+        explanation: `Man kann also eine Rechtsdrehung aus 3 Linksdrehungen erzeugen. Wenn du die Rechtsdrehung auf diese Weise häufiger nutzt, dann wird es allerdings unübersichtlich im Code. Um das zu vermeiden, kann man sogenannte "Funktionen" schreiben. Diese bündeln eine beliebige  Abfolge bereits vorhandener Befehle unter einem neuen Namen zusammen.<br><br>
+        
+        <b>Anmerkungen im Code</b><br>
+        Manchmal möchte man Notizen direkt im Code schreiben, die aber bei der Ausführung ignoriert werden sollen. In diesem Fall schreibt man // und sämtlicher Code der in der selben Zeile dahinter steht, wird ausgegraut angezeigt und in der Ausführung des Codes ignoriert.<br><br>`,
         code: `//Hier wird der neue Befehl nur definiert und 
 //von sich aus passiert noch nichts. Der Name 
 //"turnRight" ist übrigens beliebig. 
@@ -153,17 +190,16 @@ pickBeeper();`,
     },
     {
         name: "Move the Beeper RT2 - Defining functions 1",
-        explanation: `TO DO: ZEILENUMBRÜCHE 
-        Versuche nun die Funktion "turnRight" selbst zu definieren. 
+        explanation: `Versuche nun die Funktion "turnRight" selbst zu definieren.<br><br>
         
-        Tasten-Komination für die {}-Klammern: 
-        Windows: AltGr + 7 / AltGr + 0 
-        Mac: Shift + Alt + 8 / Shift + Alt + 9 
+        <b>Aufbau einer Funktion</b><br>
+        function deinFunktionsName(){<br>
+        dein Code für die Funktion...<br>
+        }<br><br>
 
-        Zur Erinnerung: so ist eine Funktion aufgebaut:
-        function deinFunktionsName(){
-        dein Code für die Funktion...
-        }`,
+        <b>Tasten-Komination für die {}-Klammern</b><br>
+        Windows: AltGr + 7 / AltGr + 0<br>
+        Mac: Shift + Alt + 8 / Shift + Alt + 9<br><br>`,
         code:
             `turnRight();
 move();
@@ -223,7 +259,9 @@ putBeeper();`,
     },
     {
         name: "Walk Across - Seeing Loops",
-        explanation: 'Manchmal muss man einen Befehl mehrfach hintereinander ausführen. Programmier*innen hassen es allerdings, sich zu wiederholen. Also nutzen sie die sogenannten Schleifen oder auch Loops, um das Problem zu lösen. Drücke auf "Run Code" und sieh was passiert.<br></br> (Ab dieser Lektion ist die Funktion "turnRight" direkt verfügbar, und muss nicht mehr manuell definiert werden)',
+        explanation: `Manchmal muss man einen Befehl mehrfach hintereinander ausführen. Programmier*innen hassen es allerdings, sich zu wiederholen. Also nutzen sie die sogenannten Schleifen oder auch Loops, um das Problem zu lösen. Drücke auf "Run Code" und sieh was passiert.<br></br>
+        
+        <i>(Ab dieser Lektion ist die Funktion "turnRight" direkt verfügbar, und muss nicht mehr manuell definiert werden.)</i><br><br>`,
         code: `move();
 pickBeeper();
 for (let i = 0; i < 5; i++) {
@@ -251,20 +289,11 @@ putBeeper();`,
     },
     {
         name: "Walk Across - Modifying Loops",
-        explanation: `Kannst du die Schleife so anpassen, dass Pfeili zur anderen Seite des Levels läuft? Wenn du dir nicht sicher bist, dann drückenochmal auf "Run Code" und sieh, ob dir ein Zusammenhang zwischen dem Code und Pfeilis Bewegung auffällt.`,
-        code: `//Kannst du die Schleife so anpassen, dass 
-//Pfeili zur anderen Seite des Levels läuft?
-//Wenn du dir nicht sicher bist, dann drücke
-//nochmal auf "Run Code" und sieh, ob dir
-//ein Zusammenhang zwischen dem Code und
-//Pfeilis Bewegung auffällt.
-
-//Es ist völlig normal, wenn du an dieser
-//Stelle noch nicht komplett verstehst, was
-//die ganzen Einzelteile in der Schleife
-//bedeuten.
-
-move();
+        explanation: `Kannst du die Schleife so anpassen, dass Pfeili zur anderen Seite des Levels läuft? Wenn du dir nicht sicher bist, dann drückenochmal auf "Run Code" und sieh, ob dir ein Zusammenhang zwischen dem Code und Pfeilis Bewegung auffällt.<br><br>
+        
+        <b>Ich verstehe nur Bahnhof.</b><br>
+        Es ist völlig normal, wenn du an dieser Stelle noch nicht verstehst, was die ganzen Einzelteile in der Schleife bedeuten. Probiere einfach erstmal etwas herum. Schleifen werden später noch in mehr Tiefe erklärt.<br><br>`,
+        code: `move();
 pickBeeper();
 for (let i = 0; i < 5; i++) {
     move();
@@ -292,11 +321,7 @@ putBeeper();`,
     {
         name: "Walk Across - Writing your own Loops",
         explanation: `Innerhalb der {}-Klammern in der Schleife, kann man auch mehrere Befehle schreiben, die allesamt in jedem Durchlauf der Schleife ausgeführt werden.`,
-        code: `//Innerhalb der {}-Klammern in der Schleife, kann man
-//auch mehrere Befehle schreiben, die allesamt in 
-//jedem Durchlauf der Schleife ausgeführt werden.
-
-for (let i = 0; i < 5; i++) {
+        code: `for (let i = 0; i < 5; i++) {
     pickBeeper();
     move();
 }`,
@@ -373,7 +398,11 @@ move();
     },
     {
         name: "Corner - Seeing If",
-        explanation: `TO DO: MULTIWORLDS ERKLÄREN Häufig wollen wir unseren Code so schreiben, dass er flexibel auf unterschiedliche Situationen reagieren kann. Dafür nutzen wir sogenannte "Bedingungen" oder "If-Statements". Diese prüfen, ob eine bestimmte Bedingung erfüllt ist und führen dann den Code innerhalb der {}-Klammern nach dem "if" nur aus, wenn die Bedingung erfüllt ist.`,
+        explanation: `<b>If-Statements</b><br>
+        wollen wir unseren Code so schreiben, dass er flexibel auf unterschiedliche Situationen reagieren kann. Dafür nutzen wir sogenannte "Bedingungen" oder "If-Statements". Diese prüfen, ob eine bestimmte Bedingung erfüllt ist und führen dann den Code innerhalb der {}-Klammern nach dem "if" nur aus, wenn die Bedingung erfüllt ist.<br><br>
+        
+        <b>Mehrere Welten</b><br>
+        Wie du siehst gibt es in diesem Level zwei Pfeilis, jeweils mit ihrem eigenen Spielfeld. In diesem Fall wird dein Code erst für die eine Welt ausgeführt und direkt im Anschluss wird derselbe Code nochmal von vorne für die andere Welt ausgeführt. Die beiden Welten beeinflussen sich gegenseitig also in keiner Form, die komplette Code-Ausführung wird auf Anfang zurückgesetzt, bevor die zweite Welt startet.<br><br>`,
         code: `move();
 pickBeeper();
 if(frontIsBlocked()) {
@@ -407,19 +436,13 @@ putBeeper();`,
     },
     {
         name: "Corner - Using If",
-        explanation: `Versuche nun selbst ein if-Statement zu schreiben. 
-Zur Erinnerung, so ist eine if-Statement aufgebaut:
-if(meineBedingung()){
-   code der ausgeführt wird, wenn die Bedingung wahr ist.
-}`,
-        code: `//Versuche nun selbst ein if-Statement
-//zu schreiben.
-//Zur Erinnerung, so ist eine if-Statement aufgebaut:
-//if(meineBedingung()){
-//   code der ausgeführt wird, wenn die Bedingung wahr ist.
-//}
-        
-move();
+        explanation: `Versuche nun selbst ein if-Statement zu schreiben.<br><br>
+
+        <b>Aufbau eines If-Statements</b><br>
+        if(meineBedingung()){<br>
+        code der ausgeführt wird, wenn die Bedingung wahr ist.<br>
+        }<br>`,
+        code: `move();
 pickBeeper();`,
         commands: ["move", "turnLeft", "turnRight", "putBeeper", "pickBeeper", "frontIsBlocked"],
         worlds: [
@@ -563,8 +586,14 @@ putBeeper();`,
     },
     {
         name: "Clean up - Using If/Else",
-        explanation: `TO DO: Wincodition wiederholt genau erklären Versuche nun selbst ein if-else-Statement zu schreiben.
-(In diesem Level startet Pfeili schon mit einem Beeper im Gepäck.)`,
+        explanation: `Versuche nun selbst ein if-else-Statement zu schreiben.<br><br>
+        
+        <i>(In diesem Level startet Pfeili schon mit einem Beeper im Gepäck)</i><br><br>
+        
+        <b>Das Ziel</b><br>
+        Jedes Level ist ein Puzzle. Um es zu lösen, musst du Anweisungen in das Textfeld schreiben und diese danach ausführen. Dabei müssen zwei Bedingungen erfüllt werden:<br> 
+        1. Alle Beeper außerhalb von Zielzonen (Die roten Rauten) müssen aufgesammelt werden.<br>
+        2. In allen markierten Zielzonen müssen Beeper abgelegt werden (diese erscheinen dann als grüne Rauten).<br><br>`,
         code: ``,
         commands: ["move", "turnLeft", "turnRight", "putBeeper", "pickBeeper", "beeperIsPresent"],
         worlds: [
